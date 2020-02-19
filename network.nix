a1d93c84c5ef7283f5296cedc095b0c1daf607ff
Remco Harmsen <rmeoc@mailbox.org>
2020-02-19 15:53:17 +0000

    Move all webserver config to webserver.nix

--- network.nix
+++ network.nix
@@ -3,25 +3,5 @@
     { config, pkgs, ... }:
     { 
       imports = [ ./webserver.nix ];
-
-      deployment.keys.client-session.keyFile = ./client-session.aes;
-      deployment.keys.client-session.user = "mywebsrv";
-      deployment.keys.client-session.group = "mywebsrv";
-      deployment.keys.client-session.permissions = "0400";
-      
-      deployment.keys.auth0-client-secret.keyFile = ./auth0-client-secret.txt;
-      deployment.keys.auth0-client-secret.user = "mywebsrv";
-      deployment.keys.auth0-client-secret.group = "mywebsrv";
-      deployment.keys.auth0-client-secret.permissions = "0400";
-
-      deployment.keys.spotify-client-secret.keyFile = ./spotify-client-secret.txt;
-      deployment.keys.spotify-client-secret.user = "mywebsrv";
-      deployment.keys.spotify-client-secret.group = "mywebsrv";
-      deployment.keys.spotify-client-secret.permissions = "0400";
-      
-      deployment.keys.tls-key.keyFile = ./tls-key.pem;
-      deployment.keys.tls-key.user = "nginx";
-      deployment.keys.tls-key.group = "nginx";
-      deployment.keys.tls-key.permissions = "0400";
     };
 }
--- webserver.nix
+++ webserver.nix
@@ -42,6 +42,26 @@ in
 
     nixpkgs.overlays = [ (import ./overlays/overlay.nix) ];
 
+    deployment.keys.client-session.keyFile = ./client-session.aes;
+    deployment.keys.client-session.user = "mywebsrv";
+    deployment.keys.client-session.group = "mywebsrv";
+    deployment.keys.client-session.permissions = "0400";
+    
+    deployment.keys.auth0-client-secret.keyFile = ./auth0-client-secret.txt;
+    deployment.keys.auth0-client-secret.user = "mywebsrv";
+    deployment.keys.auth0-client-secret.group = "mywebsrv";
+    deployment.keys.auth0-client-secret.permissions = "0400";
+
+    deployment.keys.spotify-client-secret.keyFile = ./spotify-client-secret.txt;
+    deployment.keys.spotify-client-secret.user = "mywebsrv";
+    deployment.keys.spotify-client-secret.group = "mywebsrv";
+    deployment.keys.spotify-client-secret.permissions = "0400";
+    
+    deployment.keys.tls-key.keyFile = ./tls-key.pem;
+    deployment.keys.tls-key.user = "nginx";
+    deployment.keys.tls-key.group = "nginx";
+    deployment.keys.tls-key.permissions = "0400";
+
     # Use the GRUB 2 boot loader.
     boot.loader.grub.enable = true;
     boot.loader.grub.version = 2;
